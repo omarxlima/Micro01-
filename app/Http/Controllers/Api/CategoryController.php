3f0b2470c433fc16e9919482e8d78de4bd65ef37
marxlima <marx.lima@ssp.gov.ma.br>
2023-07-13 19:27:52 -0300

    first commit

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCategory;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    protected $repository;
+    public function __construct(Category $model){
+        $this->repository = $model;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = $this->repository->get();
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUpdateCategory $request)
+    {
+
+        $category = $this->repository->create($request->validated());
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+        return new CategoryResource($category);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreUpdateCategory $request, string $url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+        $category->update($request->validated());
+        return response()->json(['message' => 'successfully updated']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+        $category->delete();
+        return response()->json([],204);
+
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('categories',CategoryController::class);
+
+Route::get( '/', function(){
+    return response()->json(['message' => 'Success']);
+});
